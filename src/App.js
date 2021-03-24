c01a9bc14f9ce7da2bc8fd3aa5600279ad008a44
AlexsandarM <59208791+AlexsandarM@users.noreply.github.com>
2021-03-24 21:27:15 +0100

    UI Ready, Next working with Redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import './App.css';
+import React, { Fragment, useEffect } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+// components
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
 
 function App() {
-  return <div>New App</div>;
+  useEffect(() => {
+    // Init Materialize JS
+    M.AutoInit();
+  });
+
+  return (
+    <Fragment>
+      <SearchBar />
+      <div className='container'>
+        <AddBtn />
+        <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <TechListModal />
+        <Logs />
+      </div>
+    </Fragment>
+  );
 }
 
 export default App;
